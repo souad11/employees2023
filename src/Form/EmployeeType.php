ad6ff1b6a089593826577b00eada8df8655eae77
Nils Henrar <nils.henrar@gmail.com>
2023-12-21 10:30:27 +0100

    Projet initial

---
+++ src/Form/EmployeeType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Gender;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('birthDate')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('gender', EnumType::class, ['class' => Gender::class])
+            ->add('hireDate')
+            ->add('photo')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
