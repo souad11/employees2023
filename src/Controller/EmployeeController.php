d4c783ac394f1991f7002ea8d772590ce808ee9a
souad <riouch.souad@gmail.com>
2024-01-09 15:55:04 +0100

    Barre de recherche + affichage du resultat

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -5,27 +5,42 @@ namespace App\Controller;
 use App\Entity\Employee;
 use App\Form\EmployeeType;
 use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 
 #[Route('/employee')]
 class EmployeeController extends AbstractController
 {
     #[Route('/', name: 'app_employee_index', methods: ['GET'])]
-    public function index(EmployeeRepository $employeeRepository): Response
+    public function index(EmployeeRepository $employeeRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $employees = $employeeRepository->findAll();
+
+        $pagination = $paginator->paginate(
+            $employees, // Requête paginée
+            $request->query->getInt('page', 1), // Numéro de la page, 1 par défaut
+            10 // Nombre d'éléments par page
+        );
+        
         return $this->render('employee/index.html.twig', [
-            'employees' => $employeeRepository->findAll(),
+            //'employees' => $employeeRepository->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 
     #[Route('/new', name: 'app_employee_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $employee = new Employee();
         $form = $this->createForm(EmployeeType::class, $employee);
        
@@ -72,6 +87,8 @@ class EmployeeController extends AbstractController
     #[Route('/{id}/edit', name: 'app_employee_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Employee $employee, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $form = $this->createForm(EmployeeType::class, $employee);
         $form->handleRequest($request);
 
@@ -99,4 +116,6 @@ class EmployeeController extends AbstractController
 
         return $this->redirectToRoute('app_employee_index', [], Response::HTTP_SEE_OTHER);
     }
+
+
 }
